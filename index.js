c74e3596dce35c682f20b1797bdd614cfdd4c5f7
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-02-05 23:02:03 +0200

    pushing first simple components training

---
+++ index.js
@@ -0,0 +1,22 @@
+// ReactDOM.render(
+//   <ul>
+//     <li>Thing1</li>
+//     <li>Thing2</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
+
+const navBar = (
+  <nav>
+    <h1>Ilias Website</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+
+);
+
+ReactDOM.render(navBar, document.getElementById("root"));
+
